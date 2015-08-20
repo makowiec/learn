fc3ae790ecc7ea2aab11ef1eb486f35d3fd02857
makowiec <ptosiek@poczta.fm>
2015-08-20 14:57:16 +0200

    - przygotowanie okna do wywołania przyciskiem;

---
+++ src/rwin.h
@@ -0,0 +1,25 @@
+#ifndef GTKMM_EXAMPLE_START_H
+#define GTKMM_EXAMPLE_START_H
+
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
+#include <gtkmm/box.h>
+
+class Startr : public Gtk::Window
+{
+
+public:
+Startr();
+virtual ~Startr();
+
+protected:
+//Signal handlers:
+void on_button_clicked_r1();
+void on_button_clicked_r2();
+
+private:
+Gtk::HBox Kontener_poziomy_r1; // Deklaracja obiektu listy poziomej
+Gtk::Button m_button_r1, m_button_r2; // Dekretacja przycisku
+};
+
+#endif // GTKMM_EXAMPLE_START_H
